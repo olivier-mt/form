f5314e21770491bf1febe085d06f7cd65aabbb34
Olivier Mountou <oliviermountou@MacBook-Pro-de-Olivier.local>
2021-04-23 20:46:27 +0200

    front

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,29 @@
 const $ = document;
 
 $.addEventListener("DOMContentLoaded", () => {
-  // votre code ici...
+  console.log("event listener working");
 
-  console.log("it's working");
+  $.querySelector("#contact-form").addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const data = {
+      firstName: $.querySelector("#firstname").value,
+      lastName: $.querySelector("#lastname").value,
+      email: $.querySelector("#email").value,
+      subject: $.querySelector("#subject").value,
+      message: $.querySelector("#message").value,
+    };
+
+    console.log(data);
+
+    try {
+      const response = await axios.post("http://localhost:3000/form", data);
+
+      console.log("data===>", data);
+      console.log(response);
+      alert("Merci pour les infos!");
+    } catch (error) {
+      console.log(error);
+    }
+  });
 });
